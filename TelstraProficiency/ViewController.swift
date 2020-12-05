4cbe90f8d1f0705f322b3e81f91432a7394e36d9
“Samuel <Samuel.Navamani@cognizant.com>
2020-12-05 15:02:12 +1100

    Initial Commit

---
+++ TelstraProficiency/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TelstraProficiency
+//
+//  Created by Navamani, Samuel on 5/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
